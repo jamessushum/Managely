b7941597ba18cb9b6b0ef3852a8781ac4e518b09
James Su-Shum <sushumjames@gmail.com>
2020-10-26 12:40:31 -0400

    Created models for UserProfile and UserType

---
+++ Managely/Models/UserProfile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Managely.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        [StringLength(28, MinimumLength = 28)]
+        public string FirebaseUserId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(555)]
+        public string Email { get; set; }
+
+        [MaxLength(255)]
+        public string Company { get; set; }
+
+        [MaxLength(255)]
+        public string ImageLocation { get; set; }
+
+        [Required]
+        public DateTime CreateDateTime { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public int UserTypeId { get; set; }
+
+        public UserType UserType { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
