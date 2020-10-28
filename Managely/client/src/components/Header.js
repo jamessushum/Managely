26420d3e1640807bf4c3f02cf084f3f1bd036db0
James Su-Shum <sushumjames@gmail.com>
2020-10-27 23:04:24 -0400

    Added navbar with dynamic links per user type

---
+++ Managely/client/src/components/Header.js
@@ -0,0 +1,78 @@
+import React, { useState, useContext } from 'react';
+import { NavLink as RRNavLink } from 'react-router-dom';
+import { UserProfileContext } from '../providers/UserProfileProvider';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+const Header = () => {
+  const { isLoggedIn, logout, isPropertyManager } = useContext(UserProfileContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <header>
+      <Navbar color="secondary" dark fixed="top" expand="lg">
+        <NavbarBrand tag={RRNavLink} to="/">Managely</NavbarBrand>
+        <NavbarToggler className="font-weight-bold bg-primary text-white rounded" onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            {isLoggedIn &&
+              <NavItem className="mx-0 mx-lg-1">
+                <NavLink tag={RRNavLink} to="/">Dashboard</NavLink>
+              </NavItem>
+            }
+          </Nav>
+          <Nav navbar>
+            {isLoggedIn && isPropertyManager &&
+              <>
+                <NavItem className="mx-0 mx-lg-1">
+                  <NavLink tag={RRNavLink} to="">Properties</NavLink>
+                </NavItem>
+                <NavItem className="mx-0 mx-lg-1">
+                  <NavLink tag={RRNavLink} to="">Work Orders</NavLink>
+                </NavItem>
+                <NavItem className="mx-0 mx-lg-1">
+                  <NavLink tag={RRNavLink} to="">Settings</NavLink>
+                </NavItem>
+                <NavItem className="mx-0 mx-lg-1">
+                  <a aria-current="page" className="nav-link"
+                    style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
+                </NavItem>
+              </>
+            }
+            {isLoggedIn && !isPropertyManager &&
+              <>
+                <NavItem className="mx-0 mx-lg-1">
+                  <NavLink tag={RRNavLink} to="">Settings</NavLink>
+                </NavItem>
+                <NavItem className="mx-0 mx-lg-1">
+                  <a aria-current="page" className="nav-link"
+                    style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
+                </NavItem>
+              </>
+            }
+            {!isLoggedIn &&
+              <>
+                <NavItem className="mx-0 mx-lg-1">
+                  <NavLink tag={RRNavLink} to="/login">Login</NavLink>
+                </NavItem>
+                <NavItem className="mx-0 mx-lg-1">
+                  <NavLink tag={RRNavLink} to="/register">Register</NavLink>
+                </NavItem>
+              </>
+            }
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header;
