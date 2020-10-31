45732a8f3366ff08eceb58045ef04b7e171e2118
James Su-Shum <sushumjames@gmail.com>
2020-10-31 15:25:31 -0400

    Created basic page structure for tenant dashboard

--- Managely/client/src/components/Dashboard/TenantDash.js
+++ Managely/client/src/components/Dashboard/TenantDash.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './TenantDash.css';
 
 const TenantDash = () => {
 
   return (
-    <div>
-      Tenant Dashboard Coming Soon!
+    <div className="tenantDash-container">
+      <div className="tenantDash-banner">
+        tenant welcome banner
+      </div>
+      <div className="tenantDash-openWO">
+        open work orders list
+      </div>
+      <div className="tenantDash-closedWO">
+        closed work orders list
+      </div>
     </div>
   )
 }
