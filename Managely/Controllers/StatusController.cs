e395935f7e1f63740d6f3b8eba50c2fe98cdc6bc
James Su-Shum <sushumjames@gmail.com>
2020-10-31 12:33:25 -0400

    Added status and severity controller with get all method

---
+++ Managely/Controllers/StatusController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Managely.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Managely.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly IStatusRepository _statusRepository;
+
+        public StatusController(IStatusRepository statusRepository)
+        {
+            _statusRepository = statusRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_statusRepository.GetAll());
+        }
+    }
+}
