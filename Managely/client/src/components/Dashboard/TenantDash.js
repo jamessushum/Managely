14ac0fe174c292a886150e72882011c871200284
James Su-Shum <sushumjames@gmail.com>
2020-10-31 15:47:17 -0400

    Setup new work order route in tenant app views

--- Managely/client/src/components/Dashboard/TenantDash.js
+++ Managely/client/src/components/Dashboard/TenantDash.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button } from 'reactstrap';
 import './TenantDash.css';
 
 const TenantDash = () => {
+  const history = useHistory();
 
   return (
     <div className="tenantDash-container">
       <div className="tenantDash-banner">
-        tenant welcome banner
+        <div>tenant welcome banner</div>
+        <div className="tenantDash-banner-addBtn">
+          <Button color="success" onClick={() => history.push('/workorder/new')}>Create New Work Order</Button>
+        </div>
       </div>
       <div className="tenantDash-openWO">
         open work orders list
---
+++ Managely/client/src/components/WorkOrder/WorkOrderAddForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WorkOrderAddForm = () => {
+
+  return (
+    <div>
+      work order add form
+    </div>
+  )
+}
+
+export default WorkOrderAddForm;
