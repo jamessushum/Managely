12ce8da6d666f221cf6258d8fb5da9b3f7504831
James Su-Shum <sushumjames@gmail.com>
2020-11-02 13:52:52 -0500

    Reused PropertyCompletedWorkOrders component to created tenant's completed work orders table in tenant dashboard

--- Managely/client/src/components/Dashboard/TenantDash.js
+++ Managely/client/src/components/Dashboard/TenantDash.js
@@ -3,6 +3,7 @@ import { WorkOrderContext } from '../../providers/WorkOrderProvider';
 import { useHistory } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import PropertyOpenWorkOrders from '../Properties/PropertyOpenWorkOrders';
+import PropertyCompletedWorkOrders from '../Properties/PropertyCompletedWorkOrders';
 import Pagination from '../../components/Pagination/Pagination';
 import './TenantDash.css';
 import { Table } from 'reactstrap';
@@ -11,22 +12,35 @@ const TenantDash = () => {
   const history = useHistory();
   const loggedInUser = JSON.parse(sessionStorage.userProfile);
 
-  const { getTenantOpenWorkOrders } = useContext(WorkOrderContext);
+  const { getTenantOpenWorkOrders, getTenantClosedWorkOrders } = useContext(WorkOrderContext);
 
   // State for tenant's open work orders component
   const [openWorkOrders, setOpenWorkOrders] = useState([]);
 
+  // State for tenant's completed work orders component
+  const [completedWorkOrders, setCompletedWorkOrders] = useState([]);
+
   // State for tenant's open work orders pagination
   const [currentPageOpenWO, setCurrentPageOpenWO] = useState(1);
   const [itemsPerPageOpenWO] = useState(5);
+
+  // State for tenant's completed work orders pagination
+  const [currentPageCompletedWO, setCurrentPageCompletedWO] = useState(1);
+  const [itemsPerPageCompletedWO] = useState(5);
 
   const getOpenWorkOrders = async () => {
     const res = await getTenantOpenWorkOrders(loggedInUser.id);
     setOpenWorkOrders(res);
   }
 
+  const getClosedWorkOrders = async () => {
+    const res = await getTenantClosedWorkOrders(loggedInUser.id);
+    setCompletedWorkOrders(res);
+  }
+
   useEffect(() => {
     getOpenWorkOrders();
+    getClosedWorkOrders();
   }, [])
 
   // Get current tenant's open work orders - for pagination
@@ -34,8 +48,16 @@ const TenantDash = () => {
   const indexOfFirstPostOpenWO = indexOfLastPostOpenWO - itemsPerPageOpenWO;
   const currentOpenWorkOrders = openWorkOrders.slice(indexOfFirstPostOpenWO, indexOfLastPostOpenWO);
 
+  // Get current tenant's completed work orders - for pagination
+  const indexOfLastPostCompletedWO = currentPageCompletedWO * itemsPerPageCompletedWO;
+  const indexOfFirstPostCompletedWO = indexOfLastPostCompletedWO - itemsPerPageCompletedWO;
+  const currentCompletedWorkOrders = completedWorkOrders.slice(indexOfFirstPostCompletedWO, indexOfLastPostCompletedWO);
+
   // Change page - for tenant's open work orders pagination
   const paginateOpenWO = (pageNumber) => setCurrentPageOpenWO(pageNumber);
+
+  // Change page - for tenant's completed work orders pagination
+  const paginateCompletedWO = (pageNumber) => setCurrentPageCompletedWO(pageNumber);
 
   return (
     <div className="tenantDash-container">
@@ -68,7 +90,26 @@ const TenantDash = () => {
         </div>
       </div>
       <div className="tenantDash-closedWO">
-        closed work orders list
+        <div className="tenantDash-closedWO-table">
+          <h4>Completed Work Orders</h4>
+          <Table>
+            <thead>
+              <tr>
+                <th>Subject</th>
+                <th>Status</th>
+                <th>Severity</th>
+                <th>Requested By</th>
+                <th>Date Created</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Reusing PropertyCompletedWorkOrders component to generate table */}
+              <PropertyCompletedWorkOrders workOrders={currentCompletedWorkOrders} />
+            </tbody>
+          </Table>
+          <Pagination itemsPerPage={itemsPerPageCompletedWO} totalItems={completedWorkOrders.length} paginate={paginateCompletedWO} />
+        </div>
       </div>
     </div>
   )
