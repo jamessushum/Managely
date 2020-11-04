cae1472a71105f81d94c9e21b4857f6221a4fd84
James Su-Shum <sushumjames@gmail.com>
2020-11-04 15:12:31 -0500

    Added welcome banner to property manager dashboard

--- Managely/client/src/components/Dashboard/PropertyManagerDash.js
+++ Managely/client/src/components/Dashboard/PropertyManagerDash.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './PropertyManagerDash.css';
 
 const PropertyManagerDash = () => {
+  const loggedInUser = JSON.parse(sessionStorage.userProfile);
+  console.log(loggedInUser)
 
   return (
-    <div>
-      Property Manager Dashboard Coming Soon!
+    <div className="propertyManagerDash-container">
+      <div className="propertyManagerDash-banner">
+        <h4>Welcome, {loggedInUser.fullName} 🚀</h4>
+      </div>
+      <div className="propertyManagerDash-openWO"></div>
+      <div className="propertyManagerDash-closedWO"></div>
     </div>
   )
 }
