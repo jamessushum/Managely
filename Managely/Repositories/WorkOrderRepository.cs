0940401c11f6104a078eb0324e32818e43dbc0c2
James Su-Shum <sushumjames@gmail.com>
2020-11-22 17:47:10 -0500

    Modified SQL query string in WorkOrderRepo GetTenantOpenWorkOrders to get all open work orders for active properties only

--- Managely/Repositories/WorkOrderRepository.cs
+++ Managely/Repositories/WorkOrderRepository.cs
@@ -478,7 +478,7 @@ namespace Managely.Repositories
 	                                        LEFT JOIN UserProfile up ON w.UserProfileId = up.Id
 	                                        LEFT JOIN Property p ON w.PropertyId = p.Id
                                         WHERE
-	                                        w.StatusId != 4 AND w.UserProfileId = @userProfileId
+	                                        w.StatusId != 4 AND w.UserProfileId = @userProfileId AND p.IsActive = 1
                                         ORDER BY
 	                                        w.CreateDateTime DESC";
 
